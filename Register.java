97145f8ab71f7c81ce6d6664ef81c1e3b64bf9fe
toraneko123 <64183615+toraneko123@users.noreply.github.com>
2020-11-30 14:14:37 +0900

    Update Register.java

--- Register.java
+++ Register.java
@@ -11,15 +11,16 @@ class Register {
         List<String> list = new ArrayList<String>();
 
         //ファイル生成
-        File newFile = new File("./Recipe/xx.txt");
-        newFile.createNewFile();
+        // File newFile = new File("./Recipe/xx.txt");
+        // newFile.createNewFile();
         
         //ファイル書き込み
-        FileWriter filewriter = new FileWriter(newFile);
+        // FileWriter filewriter = new FileWriter(newFile);
 
         System.out.println("タイトルを決めましょう");
         String title = scanner.next();
-        filewriter.write(title);
+        // filewriter.write(title);
+        
 
         System.out.println("レシピの内容を入力して下さい");
         System.out.println("終了するには exit を入力して下さい");
@@ -28,17 +29,18 @@ class Register {
             String recipes = scanner.next();
 
             if ("exit".equals(recipes)) {
+                System.out.println("タイトル：" + title);
                 System.out.println(list);
                 System.out.println("登録完了！最初に戻ります。Enterを押して下さい。");
 
                 String enter = scanner.nextLine(); 
-                filewriter.close();
+                // filewriter.close();
                 scanner.close();
                 break;
             }
 
             list.add(recipes);
-            filewriter.write(recipes);
+            // filewriter.write(recipes);
         }
     }
 }
